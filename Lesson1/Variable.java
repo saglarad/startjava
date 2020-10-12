32bc2045ee04cc8346c0227c6a7c88fa906f225e
saglarad <saglaradzham@gmail.com>
2020-10-12 19:46:38 +0300

    hw

---
+++ Lesson1/Variable.java
@@ -0,0 +1,15 @@
+public class Variable {
+	public static void main(String[] args) {
+		int cores = 4;
+		byte cacheMb = 4;
+		short tdpW = 65;
+		long speedMbS = 12800;
+		float speedOfGhz = 3.1f;
+		// char productNumber = 'F3E92AA';
+		System.out.println(cores);
+		System.out.println(cacheMb);
+		System.out.println(tdpW);
+		System.out.println(speedMbS);
+		System.out.println(speedOfGhz);
+	}
+}
