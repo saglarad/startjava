6fa8155816a00320941ceec5ef561e4edde66a16
saglarad <saglaradzham@gmail.com>
2020-10-14 22:16:52 +0300

    hw2

---
+++ Lesson2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+	String sex = "Male";
+	String name = "Pol";
+	int height = 180;
+	float weight = 92.2f;
+	int age = 30;
+
+	void move() {
+		System.out.println("Проходит в день 5000 шагов");
+	}
+
+	String sit() {
+		return "nothing";
+	}
+	
+	String run() {
+		System.out.println("Пробежал 5 км");
+		return "5";
+	}
+
+	void speak() {
+
+	}
+	boolean learnJava() {
+		System.out.println("Изучает StartJava");
+		return true;
+	}
+}
