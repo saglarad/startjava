60775251b28b255186706cce02ffcc34c2923905
saglarad <saglaradzham@gmail.com>
2020-10-16 22:24:47 +0300

    hw2

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,31 +1,75 @@
 public class Wolf {
 	
-	String gender = "Самец";
-	String name = "Серый";
-	float weight = 35.5f;
-	int age = 3;
-	String color = "серо-белый";
+	private String gender = "Самец";
+	private String name = "Серый";
+	private float weight = 35.5f;
+	private int age = 3;
+	private String color = "серо-белый";
 
-	String move() {
+	public String getGender() {
+		return gender;
+	}
+
+	void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	void setName(String name) {
+		this.name = name;
+	}
+
+	public float getWeight() {
+		return weight;
+	}
+
+	void setWeight(float weight) {
+		this.weight = weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		if(age > 8) {
+			System.out.println("Некорректный возраст");
+		} else {
+			this.age = age;
+		}
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	void setColor(String color) {
+		this.color = color;
+	}
+
+	public String move() {
 		System.out.println("Умеет ходить бесшумно");
 		return "nothing";
 	}
 
-	void sit() {
+	public void sit() {
 
 	}
 
-	int run() {
+	public int run() {
 		System.out.println("Волк бежит со скоростью 20 км в час");
 		return 20;
 	}
 
-	boolean howl() {
+	public boolean howl() {
 		System.out.println("В полнолуние воет");
 		return true;
 	}
 
-	void hunt() {
+	public void hunt() {
 
 	}
 }
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,11 +1,27 @@
 public class WolfTest {
 	public static void main(String[] args) {
 		Wolf wolfOne = new Wolf();
-		System.out.println("gender = " + wolfOne.gender);
-		System.out.println("name = " + wolfOne.name);
-		System.out.println("weight = " + wolfOne.weight);
-		System.out.println("age = " + wolfOne.age);
-		System.out.println("color = " + wolfOne.color);
+		
+		System.out.println("gender = " + wolfOne.getGender());
+		wolfOne.setGender("самка");
+		System.out.println("gender = " + wolfOne.getGender());
+		
+		System.out.println("name = " + wolfOne.getName());
+		wolfOne.setName("Серая");
+		System.out.println("name = " + wolfOne.getName());
+		
+		System.out.println("weight = " + wolfOne.getWeight());
+		wolfOne.setWeight(20);
+		System.out.println("weight = " + wolfOne.getWeight());
+		
+		System.out.println("age = " + wolfOne.getAge());
+		wolfOne.setAge(10);
+		System.out.println("age = " + wolfOne.getAge());
+		
+		System.out.println("color = " + wolfOne.getColor());
+		wolfOne.setColor("white");
+		System.out.println("color = " + wolfOne.getColor());
+		
 		wolfOne.move();
 		wolfOne.sit();
 		System.out.println(wolfOne.run());
